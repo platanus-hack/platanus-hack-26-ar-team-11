6ed0be181584644ebd679652c4e724fd3afdf62d
jgonzalezcornet <jgonzalezcornet@itba.edu.ar>
2026-05-09 05:37:13 -0300

    chore: configurar testing-library y vitest

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: { postcss: { plugins: [] } },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/test/setup.ts"],
+    css: false,
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+  },
+});
